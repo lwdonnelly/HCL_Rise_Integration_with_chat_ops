8b42a347da41886a4f0191368511d20f8a8e7639
lwdonnelly <lwdonnelly@gmail.com>
2021-02-24 20:33:58 -0500

    made program more compatible

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from urllib.parse import quote
 from resources.external_API import External_Data
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:%s@mysql.cnc.hclets.com:61606' % quote('Intern@123')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:%s@mysql.cnc.hclets.com:61606' % quote('Intern@123')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPOGATE_EXCEPTIONS'] = True
 api = Api(app)
